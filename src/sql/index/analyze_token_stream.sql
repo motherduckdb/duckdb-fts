dfcc5168f7509c2ed387ccd9e1587d3eb7092738
Denis Hirn <hirnde@gmail.com>
2026-07-30 16:23:22 +0200

    Avoid repeated analyzer term evaluation

--- src/sql/index/analyze_token_stream.sql
+++ src/sql/index/analyze_token_stream.sql
@@ -1,12 +1,12 @@
-SELECT analyzed.*
-FROM (
-    SELECT tokenized.raw_term,
-           {{term_expression}} AS term
-           {{projected_columns}}
-    FROM tokenized
-    WHERE tokenized.raw_term IS NOT NULL
-      AND tokenized.raw_term <> ''
-    {{stopwords_filter}}
-) AS analyzed
-WHERE analyzed.term IS NOT NULL
-  AND analyzed.term <> ''
+SELECT tokenized.raw_term,
+       UNNEST(
+           list_filter(
+               [{{term_expression}}],
+               lambda term: term IS NOT NULL AND term <> ''
+           )
+       ) AS term
+       {{projected_columns}}
+FROM tokenized
+WHERE tokenized.raw_term IS NOT NULL
+  AND tokenized.raw_term <> ''
+{{stopwords_filter}}
