89554eb5220037aacaf7f978d2b11b431c7a084e
Denis Hirn <hirnde@gmail.com>
2026-07-30 10:53:31 +0200

    Optimize bulk positional tokenization

--- src/sql/index/tokenize_positional_field.sql
+++ src/sql/index/tokenize_positional_field.sql
@@ -1,12 +1,11 @@
-SELECT unnest(tokens) AS w,
+SELECT token.w,
        fts_ii.rowid AS docid,
        {{field_id}} AS fieldid,
-       generate_subscripts(tokens, 1)::UINTEGER AS position
-FROM (
-    SELECT fts_ii.rowid,
-           list_filter(
-               {{fts_schema}}.tokenize(fts_ii.{{input_value}}),
-               lambda token: token IS NOT NULL AND token <> ''
-           ) AS tokens
-    FROM {{input_table}} AS fts_ii
-) AS fts_ii
+       token.position::UINTEGER AS position
+FROM {{input_table}} AS fts_ii
+CROSS JOIN LATERAL UNNEST(
+    list_filter(
+        {{fts_schema}}.tokenize(fts_ii.{{input_value}}),
+        lambda value: value IS NOT NULL AND value <> ''
+    )
+) WITH ORDINALITY AS token(w, position)
